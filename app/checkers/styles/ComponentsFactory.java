0cc8869d78a98fb35532563bb92cc5ed55d9b594
MrXacx <misterxacx@gmail.com>
2023-04-29 15:17:15 -0300

    Densenvolve criação do tabuleiro

--- app/checkers/styles/ComponentsFactory.java
+++ app/checkers/styles/ComponentsFactory.java
@@ -3,22 +3,34 @@ package app.checkers.styles;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 
-public class ComponentsFactory extends JPanel{
+import java.awt.Color;
+
+public class ComponentsFactory{
+	private List<Color> color =  Arrays.asList(Color.BLACK, Color.WHITE);
 	protected JButton[][] checkerboard;
     
-    public JButton[][] createCheckerboard( JButton[][] square){
-		for(JButton[] line : square){ // Itera linhas do array
-			for(JButton column: line){ // itera colunas da linha do array
+    protected JButton[][] createCheckerboard( JButton[][] square){
+    	
+		for(int line = 0; line < square.lenght; line++){
+			for(int column = 0; column < square.lenght; column++){ 
 			
-				column = new JButton(); // inicia botão
-				column.addActionListener(evt -> {
+				square[line][column]  = styleButton(new JButton()); // Inicia botão
+				square[line][column].addActionListener(evt -> {
 					// Evento
 				});
 				
 			}
+			Collections.reverse(color); // Inverte posições do array das cores
 		}
 		return square;
         
-	} 
+	}
+	public JButton styleButton(JButton button){
+	
+		button.setBackground (color.get(column % 2)); // Define cor do background de acordo com o módulo
+		button.setFocusPainted(false); // Retira foco
+		
+		return button;
+	}
 }
 
